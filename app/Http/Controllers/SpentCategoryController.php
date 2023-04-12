96808045eb4e597c4cd5c4432984d894a3bf9464
125mr <r.murakami@engenius.co.jp>
2023-04-12 11:42:59 +0900

    controller,routes作成

---
+++ app/Http/Controllers/SpentCategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\SpentCategory;
+
+class SpentCategoryController extends Controller
+{
+    public function index()
+    {
+        // デフォルトのカテゴリー
+        $defaultCategory = SpentCategory::whereNull('user_id')->get();
+
+        // ユーザーが追加したカテゴリー
+        $addCategory = SpentCategory::where('user_id', Auth::user()->id)->get();
+
+        return response()->json([
+            'defaultCategory' => $defaultCategory,
+            'addCategory' => $addCategory
+        ]);
+    }
+}
